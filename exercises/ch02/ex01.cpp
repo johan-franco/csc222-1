95d3e045f215eaccca5d994c897fd17727410d99
Jose Ejemplo <jelkner@yahoo.com>
2023-09-06 10:20:00 -0400

    Finish ch2 ex1

--- exercises/ch02/ex01.cpp
+++ exercises/ch02/ex01.cpp
@@ -1,18 +1,16 @@
 int main() {
-   int hour, minute;
-   char colon;
+    int hour, minute;
+    char colon;
 
-   hour = 11;
-   minute = 59;
-   colon = ':';
+    hour = 11;
+    minute = 59;
+    colon = ':';
 
-   cout << "The current time is ";
-   cout << hour;
-   cout << colon;
-   cout << minute;
-   cout << endl;
+    cout << "The current time is ";
+    cout << hour;
+    cout << colon;
+    cout << minute;
+    cout << endl;
 
-   return 0;
+    return 0;
 }
-
-
