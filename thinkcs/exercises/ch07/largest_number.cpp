2585fa05943256dbd7d9b415623cce733992ff71
Jose Ejemplo <jelkner@yahoo.com>
2023-09-21 07:30:02 -0400

    Add largest number example

---
+++ thinkcs/exercises/ch07/largest_number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int count = 0;
+    int largest, num;
+
+    for (int i = 0; i < 20; i++ ) {
+        cin >> num;
+        cout << "The number is: " << num << ".\n";
+    }
+
+    return 0;
+}
